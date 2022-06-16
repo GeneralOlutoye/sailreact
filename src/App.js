217180e5ccfd583d306fff43de07ac87d547b5a2
GeneralOlutoye <donronald27@gmail.com>
2022-06-16 14:18:06 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import Suli from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state= {
+    people: [
+      {name: 'Raul', age: 70},
+      {name: 'Figo', age: 30},
+      {name: 'Carlos', age: 50}
+    ]
+  }
+  switchNameHandler=(serLeaf)=>{
+    this.setState({
+      people: [
+        {name: 'Leo', age: 27},
+        {name: serLeaf, age: 30},
+        {name: 'Kaka', age: 37}
+      ]
+    })
+  }
+
+  nameChangeHandler=(event)=>{
+    this.setState({
+      people: [
+        {name: event.target.value, age: 27},
+        {name: 'serLeaf', age: 30},
+        {name: 'Kaka', age: 37}
+      ]
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>
+          General Don Turn React Developer
+        </h1>
+        <Suli name= {this.state.people[0].name} age ={this.state.people[0].age} zeh={this.nameChangeHandler} />
+        <Suli name= {this.state.people[1].name} age ={this.state.people[1].age} caleb={()=>this.switchNameHandler('Ronaldo')} />
+        <Suli name= {this.state.people[2].name} age ={this.state.people[2].age} />
+
+
+        <button onClick={this.switchNameHandler.bind(this, )}>SWITCH</button>
+      </div>
+    )   
+  }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Person = (props) => {
+  return (
+    <div>
+        <p onClick={props.caleb}>My name na {props.name} and i be {Math.floor(Math.random()*(30 - 18 + 1) + 18)} years old </p>
+        <input value={props.name} onChange={props.zeh}></input>
+     </div>
+  )
+}
+
+export default Person
